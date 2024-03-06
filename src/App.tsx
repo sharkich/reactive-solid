60c887970f17824d72a228821bd75079b74bcdcc
Artem Koziar <koziar.artem@gmail.com>
2024-03-06 12:23:01 +0200

    SOLID DIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from 'react';
 
-import { ArtCard, Card } from './Card';
+import { CardTemplate, SelfLoadingCard } from './Card';
 import { ArtDescription } from './components/ArtDescription';
+import { useArt } from './useArt';
+import { useCard } from './useCard';
 
 export const App: FC = () => (
   <div className="App">
     <main className="cards">
-      <Card />
-      <ArtCard
-        renderDescriptionComponent={({ size, subtitle, author }) => (
-          <ArtDescription author={author} size={size} subtitle={subtitle} />
+      <SelfLoadingCard useCardHook={useCard} />
+      <SelfLoadingCard
+        renderCardTemplate={card => (
+          <CardTemplate
+            card={card}
+            renderDescriptionComponent={({ size, subtitle, author }) => (
+              <ArtDescription author={author} size={size} subtitle={subtitle} />
+            )}
+          />
         )}
+        useCardHook={useArt}
       />
     </main>
   </div>
--- src/Card.tsx
+++ src/Card.tsx
@@ -5,39 +5,43 @@ import { Description } from './components/Description';
 import { Header } from './components/Header';
 import { Image } from './components/Image';
 import { Loading } from './components/Loading';
-import { useArt } from './useArt';
-import { useCard } from './useCard';
 
-interface Props<T extends ICard> {
+interface CardTemplateProps<T extends ICard> {
+  card: T;
   renderHeaderComponent?: (card: T) => ReactNode;
   renderImageComponent?: (card: T) => ReactNode;
   renderDescriptionComponent?: (card: T) => ReactNode;
+}
+
+export const CardTemplate = <T extends ICard>({
+  card,
+  renderHeaderComponent = ({ title }) => <Header title={title} />,
+  renderImageComponent = ({ title, image }) => <Image image={image} title={title} />,
+  renderDescriptionComponent = ({ subtitle, size }) => <Description size={size} subtitle={subtitle} />
+}: CardTemplateProps<T>) => (
+  <div className="card">
+    {renderHeaderComponent(card)}
+    {renderImageComponent(card)}
+    {renderDescriptionComponent(card)}
+  </div>
+);
+
+interface Props<T extends ICard> {
+  useCardHook: () => { card: T | null };
+  renderCardTemplate?: (card: T) => ReactNode;
   LoadingComponent?: FC;
 }
 
-const getCard =
-  <T extends ICard>(useCardHook: () => { card: T | null }) =>
-  ({
-    renderHeaderComponent = ({ title }) => <Header title={title} />,
-    renderImageComponent = ({ title, image }) => <Image image={image} title={title} />,
-    renderDescriptionComponent = ({ subtitle, size }) => <Description size={size} subtitle={subtitle} />,
-    LoadingComponent = Loading
-  }: Props<T>) => {
-    const { card } = useCardHook();
+export const SelfLoadingCard = <T extends ICard>({
+  useCardHook,
+  renderCardTemplate = card => <CardTemplate card={card} />,
+  LoadingComponent = Loading
+}: Props<T>) => {
+  const { card } = useCardHook();
 
-    return (
-      <div>
-        {!card && <LoadingComponent />}
-        {card && (
-          <div className="card">
-            {renderHeaderComponent(card)}
-            {renderImageComponent(card)}
-            {renderDescriptionComponent(card)}
-          </div>
-        )}
-      </div>
-    );
-  };
+  if (!card) {
+    return <LoadingComponent />;
+  }
 
-export const Card = getCard(useCard);
-export const ArtCard = getCard(useArt);
+  return renderCardTemplate(card);
+};
