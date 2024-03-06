491bb9af18c0d3bba26532094aa98b782798446d
Artem Koziar <koziar.artem@gmail.com>
2024-03-06 11:00:31 +0200

    SOLID OCP

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,21 +1,37 @@
 import { FC } from 'react';
 
+import { ICard } from './card.interfaces';
 import { Description } from './components/Description';
 import { Header } from './components/Header';
 import { Image } from './components/Image';
+import { Loading } from './components/Loading';
 import { useCard } from './useCard';
 
-export const Card: FC = () => {
-  const { card } = useCard();
+interface Props {
+  useCardHook?: typeof useCard;
+  HeaderComponent?: FC<{ card: ICard }>;
+  ImageComponent?: FC<{ card: ICard }>;
+  DescriptionComponent?: FC<{ card: ICard }>;
+  LoadingComponent?: FC;
+}
+
+export const Card: FC<Props> = ({
+  useCardHook = useCard,
+  HeaderComponent = Header,
+  ImageComponent = Image,
+  DescriptionComponent = Description,
+  LoadingComponent = Loading
+}) => {
+  const { card } = useCardHook();
 
   return (
     <div>
-      {!card && <p>Loading...</p>}
+      {!card && <LoadingComponent />}
       {card && (
         <div className="card">
-          <Header card={card} />
-          <Image card={card} />
-          <Description card={card} />
+          <HeaderComponent card={card} />
+          <ImageComponent card={card} />
+          <DescriptionComponent card={card} />
         </div>
       )}
     </div>
---
+++ src/components/Loading.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+
+export const Loading: FC = () => <p>Loading...</p>;
