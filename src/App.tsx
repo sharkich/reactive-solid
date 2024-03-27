97c97c61c54d4bbaa759f0f7c187481af43f104f
Artem Koziar <koziar.artem@gmail.com>
2024-03-27 11:06:02 +0200

    Add batchCards

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
+import { getArt, getCard } from './api';
 import { CardTemplate, SelfLoadingCard } from './Card/Card';
+import { IArt, ICard } from './Card/card.interfaces';
+import { AnyCardTemplate } from './Card/components/AnyCardTemplate';
 import { ArtDescription } from './Card/components/ArtDescription';
+import { Loading } from './Card/components/Loading';
 import { useArt } from './Card/useArt';
 import { useCard } from './Card/useCard';
 
-export const App: FC = () => (
-  <div className="App">
-    <main className="cards">
-      <SelfLoadingCard useCardHook={useCard} />
-      <SelfLoadingCard
-        renderCardTemplate={card => (
-          <CardTemplate
-            card={card}
-            renderDescriptionComponent={({ size, subtitle, author }) => (
-              <ArtDescription author={author} size={size} subtitle={subtitle} />
-            )}
-          />
-        )}
-        useCardHook={useArt}
-      />
-    </main>
-  </div>
-);
+export const App: FC = () => {
+  const [batchCards, setBatchCards] = useState<Array<ICard | IArt>>([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      setBatchCards(await Promise.all([await getCard(), await getCard(), await getArt(), await getArt()]));
+    };
+    void fetchCards();
+  }, []);
+
+  return (
+    <div className="App">
+      <main className="cards">
+        <SelfLoadingCard useCardHook={useCard} />
+        <SelfLoadingCard
+          renderCardTemplate={card => (
+            <CardTemplate
+              card={card}
+              renderDescriptionComponent={({ size, subtitle, author }) => (
+                <ArtDescription author={author} size={size} subtitle={subtitle} />
+              )}
+            />
+          )}
+          useCardHook={useArt}
+        />
+      </main>
+
+      <div className="cards">
+        {!batchCards.length && <Loading />}
+        {batchCards.map(card => (
+          <AnyCardTemplate key={card.id} card={card} />
+        ))}
+      </div>
+    </div>
+  );
+};
